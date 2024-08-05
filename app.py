b68c9beb5a1a2019d934ae916a18dcee230ed88f
Tienda-2024 <mhuaman@globokas.com>
2024-08-04 22:47:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,43 @@
 import streamlit as st
 from pymongo import MongoClient, errors
 
+# Estilo CSS para mejorar la apariencia
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: #f0f2f6;
+        
+    }
+    .css-1d391kg {
+        background-color: #f0f2f6;
+    }
+    .css-10trblm {
+        background-color: #ffffff;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    .stButton>button {
+        background-color: #007bff;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        padding: 10px;
+    }
+    .stButton>button:hover {
+        background-color: #0056b3;
+        color: white;
+    }
+    .stTextInput>div>div>input {
+        border: 2px solid #007bff;
+        border-radius: 5px;
+        padding: 10px;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
 # Intentar conectar a MongoDB
 try:
     client = MongoClient("mongodb+srv://mhuaman:0AcY7h5YMFqWCvRS@innova.gfmnmzd.mongodb.net/?retryWrites=true&w=majority&appName=Innova")
